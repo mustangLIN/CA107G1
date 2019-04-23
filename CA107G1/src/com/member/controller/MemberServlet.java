856b64bfffd4206e43506205ba7c715c0ff95a65
mustangLIN <mustang.x.lin@gmail.com>
2019-04-23 21:43:12 +0800

    test222

--- CA107G1/src/com/member/controller/MemberServlet.java
+++ CA107G1/src/com/member/controller/MemberServlet.java
@@ -1,6 +1,6 @@
 package com.member.controller;
 
-<<<<<<< HEAD
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
@@ -18,51 +18,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MemberServlet extends HttpServlet {
-	private final static String CONTENT_TYPE = "text/html; charset=UTF-8";
-
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		req.setCharacterEncoding("UTF-8");
-		Gson gson = new Gson();
-		BufferedReader br = req.getReader();
-		StringBuilder jsonIn = new StringBuilder();
-		String line = null;
-		while ((line = br.readLine()) != null) {
-			jsonIn.append(line);
-		}
-		System.out.println("input: " + jsonIn);
-		MemberService memSvc = new MemberService();
-		MemberDAO memberDAO = new MemberDAO();
-		JsonObject jsonObject = gson.fromJson(jsonIn.toString(), JsonObject.class);
-		String action = jsonObject.get("action").getAsString();
-
-		if ("isMember".equals(action)) {
-			String mem_account = jsonObject.get("mem_account").getAsString();
-			String mem_password = jsonObject.get("mem_password").getAsString();
-			writeText(res, String.valueOf(memSvc.isMember(mem_account, mem_password)));
-		} else if ("isUserIdExist".equals(action)) {
-			String mem_account = jsonObject.get("userId").getAsString();
-			writeText(res, String.valueOf(memSvc.isUserIdExist(mem_account)));
-		}else if("insert".equals(action)) {
-			MemberVO memberVO = gson.fromJson(jsonObject.get("memberVO").getAsString(), MemberVO.class);
-			writeText(res, String.valueOf(memberDAO.insert(memberVO)));
-		}
-	}
-
-	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		doPost(req, res);
-	}
-
-	private void writeText(HttpServletResponse res, String outText) throws IOException {
-		res.setContentType(CONTENT_TYPE);
-		PrintWriter out = res.getWriter();
-		out.print(outText);
-		out.close();
-		System.out.println("outText: " + outText);
-
-	}
-}
-=======
 import java.io.*;
 import java.util.*;
 
--- CA107G1/src/com/tool/TestIMG.java
+++ CA107G1/src/com/tool/TestIMG.java
@@ -6,6 +6,7 @@ import java.io.*;
 
 //import com.member.model.MemberJDBCDAO;
 //import com.member.model.MemberVO;
+// test git
 
 public class TestIMG {
 	
